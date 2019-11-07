7d167ce4b161821c3661d02664d401154de143d9
BrittnyDesir <bberry.336@gmail.com>
2019-11-07 18:29:59 -0500

    Chap 3 Ex 2

---
+++ DataTypeSize.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  DataTypeSize
+//  This exercise compares the sizes of different data types in C++
+//
+//  Created by Brittny Desir on 11/7/19.
+//  Copyright © 2019 Brittny Desir. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "The size of an unsigned int in bytes is: " << sizeof(unsigned int) << endl;
+    cout << "The size of an int in bytes is: " << sizeof(int) << endl;
+    cout << "The size of a long int in bytes is: " << sizeof(long int) << endl;
+
+    return 0;
+}
